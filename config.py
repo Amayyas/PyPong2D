fcd9767a3091d4c299c6c31c522ca144469ea940
Amayyas <amayyas.aouadene@epitech.eu>
2024-11-22 15:07:45 +0100

    [ADD] config of the 2Dpong game

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+
+# === Constants ===
+WIDTH, HEIGHT = 800, 600
+DIMENSIONS = (WIDTH, HEIGHT)
+
+# Couleurs
+COLORS = {
+    'yellow': (255, 255, 191),
+    'blue': (173, 216, 230),
+    'green': (173, 255, 47),
+    'purple': (221, 160, 221)
+}
+
+# Propriétés des paddles
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+PADDLE_SPEED = 7
+
+# Propriétés de la balle
+BALL_SIZE = 30
+BALL_SPEED = 5
+
+# Initialisation de la police
+FONT_NAME = "Comic Sans MS"
+FONT_SIZE = 30
